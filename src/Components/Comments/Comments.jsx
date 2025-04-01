214b42f5901c189ada3e594f2eb31046119b5ebf
Madina-Web-Developer <developermadina761@gmail.com>
2025-04-01 12:21:49 +0600

    add review bg

--- src/Components/Comments/Comments.jsx
+++ src/Components/Comments/Comments.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Comment.css'
-import revimg from '../../assets/RV.png'
+import revimg from '../../assets/RV3.png'
 
 
 const Comments = () => {
   return (
     <section id='Review'style={{background:`url(${revimg})center/cover no-repeat`}} className='review_madina'>
+
+      
 
     </section>
   )
